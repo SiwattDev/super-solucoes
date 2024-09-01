2df8d065cd345600a3da1ceafed7d291eb62c057
Vanorton <siwatt.oficial@gmail.com>
2024-08-31 21:53:04 -0300

    Finalizado

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,12 @@
-import { ChangeEvent, useState } from 'react'
+import {
+    formatToCEP,
+    formatToCNPJ,
+    formatToCPF,
+    isCEP,
+    isCNPJ,
+    isCPF,
+} from 'brazilian-values'
+import React, { ChangeEvent, useState } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -26,10 +34,12 @@ const FormWrapper = styled.form`
     max-width: 800px;
 `
 
-const InputWrapper = styled.div`
+const InputWrapper = styled.div<{ hasError?: boolean }>`
     display: flex;
     flex-direction: column;
     margin-bottom: 20px;
+    border: ${(props) => (props.hasError ? '1px solid red' : 'none')};
+    padding: ${(props) => (props.hasError ? '8px' : '0')};
 `
 
 const Input = styled.input`
@@ -62,6 +72,12 @@ const Button = styled.button`
     }
 `
 
+const ErrorMessage = styled.span`
+    color: red;
+    font-size: 0.875rem;
+    margin-top: 5px;
+`
+
 interface FormData {
     businessName: string
     cnpj: string
@@ -80,6 +96,10 @@ interface FormData {
     individualPhone: string
     individualEmail: string
     sameAddress: boolean
+}
+
+interface FormErrors {
+    [key: string]: string
 }
 
 export default function Form() {
@@ -103,20 +123,154 @@ export default function Form() {
         sameAddress: true,
     })
 
+    const [formErrors, setFormErrors] = useState<FormErrors>({})
+
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const { id, value } = e.target
-        setFormData({ ...formData, [id]: value })
+        let formattedValue = value
+
+        if (id === 'cnpj') {
+            formattedValue = formatToCNPJ(value)
+        } else if (id === 'cpf') {
+            formattedValue = formatToCPF(value)
+        } else if (id === 'legalZipCode' || id === 'individualZipCode') {
+            formattedValue = formatToCEP(value)
+        }
+
+        setFormData({ ...formData, [id]: formattedValue })
     }
 
     const handleCheckboxChange = () => {
         setFormData({ ...formData, sameAddress: !formData.sameAddress })
     }
 
+    const createWhatsAppMessage = (formData: FormData) => {
+        const {
+            businessName,
+            cnpj,
+            legalNeighborhood,
+            legalCity,
+            legalState,
+            legalZipCode,
+            legalPhone,
+            legalEmail,
+            responsibleName,
+            cpf,
+            individualNeighborhood,
+            individualCity,
+            individualState,
+            individualZipCode,
+            individualPhone,
+            individualEmail,
+            sameAddress,
+        } = formData
+
+        let message = `Olá, estou interessado no modelo de licença de marca da Super Soluções. Seguem os dados informados:\n\n`
+
+        message += `Razão Social: ${businessName}\n`
+        message += `CNPJ: ${cnpj}\n`
+        message += `Bairro Jurídico: ${legalNeighborhood}\n`
+        message += `Cidade Jurídica: ${legalCity}\n`
+        message += `Estado Jurídico: ${legalState}\n`
+        message += `CEP Jurídico: ${legalZipCode}\n`
+        message += `Telefone Jurídico: ${legalPhone}\n`
+        message += `E-mail Jurídico: ${legalEmail}\n`
+        message += `Nome do Responsável: ${responsibleName}\n`
+        message += `CPF: ${cpf}\n`
+        message += `Telefone (Pessoa Física): ${individualPhone}\n`
+        message += `E-mail (Pessoa Física): ${individualEmail}\n`
+
+        if (!sameAddress) {
+            message += `\nEndereço Físico:\n`
+            message += `Bairro (Pessoa Física): ${individualNeighborhood}\n`
+            message += `Cidade (Pessoa Física): ${individualCity}\n`
+            message += `Estado (Pessoa Física): ${individualState}\n`
+            message += `CEP (Pessoa Física): ${individualZipCode}\n`
+        } else {
+            message += `\nO endereço físico e jurídico são iguais.\n`
+        }
+
+        return encodeURIComponent(message)
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const errors: FormErrors = {}
+        const requiredFields = [
+            'businessName',
+            'cnpj',
+            'legalNeighborhood',
+            'legalCity',
+            'legalState',
+            'legalZipCode',
+            'legalPhone',
+            'legalEmail',
+            'responsibleName',
+            'cpf',
+            'individualPhone',
+            'individualEmail',
+        ]
+        requiredFields.forEach((field) => {
+            if (!formData[field as keyof FormData]) {
+                errors[field] = 'Este campo é obrigatório'
+            }
+        })
+
+        if (!isCNPJ(formData.cnpj)) {
+            errors.cnpj = 'CNPJ inválido'
+        }
+
+        if (!isCPF(formData.cpf)) {
+            errors.cpf = 'CPF inválido'
+        }
+
+        if (!isCEP(formData.legalZipCode)) errors.legalZipCode = 'CEP inválido'
+
+        if (!formData.sameAddress) {
+            if (!isCEP(formData.individualZipCode))
+                errors.individualZipCode = 'CEP inválido'
+        }
+
+        if (Object.keys(errors).length) {
+            setFormErrors(errors)
+        } else {
+            // Gerar mensagem para WhatsApp
+            const message = createWhatsAppMessage(formData)
+            const whatsappUrl = `https://wa.me/5577999919494?text=${message}`
+
+            // Redirecionar para a URL do WhatsApp
+            window.location.href = whatsappUrl
+
+            // Limpar erros e dados do formulário, se necessário
+            setFormErrors({})
+            setFormData({
+                businessName: '',
+                cnpj: '',
+                legalNeighborhood: '',
+                legalCity: '',
+                legalState: '',
+                legalZipCode: '',
+                legalPhone: '',
+                legalEmail: '',
+                responsibleName: '',
+                cpf: '',
+                individualNeighborhood: '',
+                individualCity: '',
+                individualState: '',
+                individualZipCode: '',
+                individualPhone: '',
+                individualEmail: '',
+                sameAddress: true,
+            })
+        }
+    }
+
     return (
         <Container id='form'>
             <Title>Formulário de Interesse</Title>
-            <FormWrapper>
-                <InputWrapper>
+            <FormWrapper onSubmit={handleSubmit}>
+                <InputWrapper hasError={!!formErrors.businessName}>
                     <label htmlFor='businessName'>Razão Social:</label>
                     <Input
                         id='businessName'
@@ -124,8 +278,11 @@ export default function Form() {
                         value={formData.businessName}
                         onChange={handleChange}
                     />
+                    {formErrors.businessName && (
+                        <ErrorMessage>{formErrors.businessName}</ErrorMessage>
+                    )}
                 </InputWrapper>
-                <InputWrapper>
+                <InputWrapper hasError={!!formErrors.cnpj}>
                     <label htmlFor='cnpj'>CNPJ:</label>
                     <Input
                         id='cnpj'
@@ -133,17 +290,25 @@ export default function Form() {
                         value={formData.cnpj}
                         onChange={handleChange}
                     />
+                    {formErrors.cnpj && (
+                        <ErrorMessage>{formErrors.cnpj}</ErrorMessage>
+                    )}
                 </InputWrapper>
-                <InputWrapper>
-                    <label htmlFor='legalNeighborhood'>Bairro Jurídico:</label>
+                <InputWrapper hasError={!!formErrors.legalNeighborhood}>
+                    <label htmlFor='legalNeighborhood'>Bairro:</label>
                     <Input
                         id='legalNeighborhood'
                         type='text'
                         value={formData.legalNeighborhood}
                         onChange={handleChange}
                     />
+                    {formErrors.legalNeighborhood && (
+                        <ErrorMessage>
+                            {formErrors.legalNeighborhood}
+                        </ErrorMessage>
+                    )}
                 </InputWrapper>
-                <InputWrapper>
+                <InputWrapper hasError={!!formErrors.legalCity}>
                     <label htmlFor='legalCity'>Cidade Jurídica:</label>
                     <Input
                         id='legalCity'
@@ -151,44 +316,59 @@ export default function Form() {
                         value={formData.legalCity}
                         onChange={handleChange}
                     />
+                    {formErrors.legalCity && (
+                        <ErrorMessage>{formErrors.legalCity}</ErrorMessage>
+                    )}
                 </InputWrapper>
-                <InputWrapper>
-                    <label htmlFor='legalState'>Estado Jurídico:</label>
+                <InputWrapper hasError={!!formErrors.legalState}>
+                    <label htmlFor='legalState'>Estado:</label>
                     <Input
                         id='legalState'
                         type='text'
                         value={formData.legalState}
                         onChange={handleChange}
                     />
+                    {formErrors.legalState && (
+                        <ErrorMessage>{formErrors.legalState}</ErrorMessage>
+                    )}
                 </InputWrapper>
-                <InputWrapper>
-                    <label htmlFor='legalZipCode'>CEP Jurídico:</label>
+                <InputWrapper hasError={!!formErrors.legalZipCode}>
+                    <label htmlFor='legalZipCode'>CEP:</label>
                     <Input
                         id='legalZipCode'
                         type='text'
                         value={formData.legalZipCode}
                         onChange={handleChange}
                     />
+                    {formErrors.legalZipCode && (
+                        <ErrorMessage>{formErrors.legalZipCode}</ErrorMessage>
+                    )}
                 </InputWrapper>
-                <InputWrapper>
-                    <label htmlFor='legalPhone'>Telefone Jurídico:</label>
+                <InputWrapper hasError={!!formErrors.legalPhone}>
+                    <label htmlFor='legalPhone'>Telefone:</label>
                     <Input
                         id='legalPhone'
                         type='text'
                         value={formData.legalPhone}
                         onChange={handleChange}
                     />
+                    {formErrors.legalPhone && (
+                        <ErrorMessage>{formErrors.legalPhone}</ErrorMessage>
+                    )}
                 </InputWrapper>
-                <InputWrapper>
-                    <label htmlFor='legalEmail'>E-mail Jurídico:</label>
+                <InputWrapper hasError={!!formErrors.legalEmail}>
+                    <label htmlFor='legalEmail'>E-mail:</label>
                     <Input
                         id='legalEmail'
                         type='email'
                         value={formData.legalEmail}
                         onChange={handleChange}
                     />
+                    {formErrors.legalEmail && (
+                        <ErrorMessage>{formErrors.legalEmail}</ErrorMessage>
+                    )}
                 </InputWrapper>
-                <InputWrapper>
+                <InputWrapper hasError={!!formErrors.responsibleName}>
                     <label htmlFor='responsibleName'>
                         Nome do Responsável:
                     </label>
@@ -198,8 +378,13 @@ export default function Form() {
                         value={formData.responsibleName}
                         onChange={handleChange}
                     />
+                    {formErrors.responsibleName && (
+                        <ErrorMessage>
+                            {formErrors.responsibleName}
+                        </ErrorMessage>
+                    )}
                 </InputWrapper>
-                <InputWrapper>
+                <InputWrapper hasError={!!formErrors.cpf}>
                     <label htmlFor='cpf'>CPF:</label>
                     <Input
                         id='cpf'
@@ -207,34 +392,17 @@ export default function Form() {
                         value={formData.cpf}
                         onChange={handleChange}
                     />
-                </InputWrapper>
-                <InputWrapper>
-                    <label htmlFor='individualPhone'>
-                        Telefone Pessoa Física:
-                    </label>
-                    <Input
-                        id='individualPhone'
-                        type='text'
-                        value={formData.individualPhone}
-                        onChange={handleChange}
-                    />
-                </InputWrapper>
-                <InputWrapper>
-                    <label htmlFor='individualEmail'>
-                        E-mail Pessoa Física:
-                    </label>
-                    <Input
-                        id='individualEmail'
-                        type='email'
-                        value={formData.individualEmail}
-                        onChange={handleChange}
-                    />
+                    {formErrors.cpf && (
+                        <ErrorMessage>{formErrors.cpf}</ErrorMessage>
+                    )}
                 </InputWrapper>
                 {!formData.sameAddress && (
-                    <>
-                        <InputWrapper>
+                    <React.Fragment>
+                        <InputWrapper
+                            hasError={!!formErrors.individualNeighborhood}
+                        >
                             <label htmlFor='individualNeighborhood'>
-                                Bairro Pessoa Física:
+                                Bairro (Pessoa Física):
                             </label>
                             <Input
                                 id='individualNeighborhood'
@@ -242,10 +410,15 @@ export default function Form() {
                                 value={formData.individualNeighborhood}
                                 onChange={handleChange}
                             />
+                            {formErrors.individualNeighborhood && (
+                                <ErrorMessage>
+                                    {formErrors.individualNeighborhood}
+                                </ErrorMessage>
+                            )}
                         </InputWrapper>
-                        <InputWrapper>
+                        <InputWrapper hasError={!!formErrors.individualCity}>
                             <label htmlFor='individualCity'>
-                                Cidade Física:
+                                Cidade (Pessoa Física):
                             </label>
                             <Input
                                 id='individualCity'
@@ -253,10 +426,15 @@ export default function Form() {
                                 value={formData.individualCity}
                                 onChange={handleChange}
                             />
+                            {formErrors.individualCity && (
+                                <ErrorMessage>
+                                    {formErrors.individualCity}
+                                </ErrorMessage>
+                            )}
                         </InputWrapper>
-                        <InputWrapper>
+                        <InputWrapper hasError={!!formErrors.individualState}>
                             <label htmlFor='individualState'>
-                                Estado Pessoa Física:
+                                Estado (Pessoa Física):
                             </label>
                             <Input
                                 id='individualState'
@@ -264,32 +442,75 @@ export default function Form() {
                                 value={formData.individualState}
                                 onChange={handleChange}
                             />
+                            {formErrors.individualState && (
+                                <ErrorMessage>
+                                    {formErrors.individualState}
+                                </ErrorMessage>
+                            )}
                         </InputWrapper>
-                        <InputWrapper>
+                        <InputWrapper hasError={!!formErrors.individualZipCode}>
                             <label htmlFor='individualZipCode'>
-                                CEP Pessoa Física:
+                                CEP (Pessoa Física):
                             </label>
                             <Input
                                 id='individualZipCode'
                                 type='text'
                                 value={formData.individualZipCode}
                                 onChange={handleChange}
+                                disabled={formData.sameAddress}
                             />
+                            {formErrors.individualZipCode && (
+                                <ErrorMessage>
+                                    {formErrors.individualZipCode}
+                                </ErrorMessage>
+                            )}
                         </InputWrapper>
-                    </>
+                    </React.Fragment>
                 )}
+                <InputWrapper hasError={!!formErrors.individualPhone}>
+                    <label htmlFor='individualPhone'>
+                        Telefone (Pessoa Física):
+                    </label>
+                    <Input
+                        id='individualPhone'
+                        type='text'
+                        value={formData.individualPhone}
+                        onChange={handleChange}
+                    />
+                    {formErrors.individualPhone && (
+                        <ErrorMessage>
+                            {formErrors.individualPhone}
+                        </ErrorMessage>
+                    )}
+                </InputWrapper>
+                <InputWrapper hasError={!!formErrors.individualEmail}>
+                    <label htmlFor='individualEmail'>
+                        E-mail (Pessoa Física):
+                    </label>
+                    <Input
+                        id='individualEmail'
+                        type='email'
+                        value={formData.individualEmail}
+                        onChange={handleChange}
+                    />
+                    {formErrors.individualEmail && (
+                        <ErrorMessage>
+                            {formErrors.individualEmail}
+                        </ErrorMessage>
+                    )}
+                </InputWrapper>
                 <CheckboxWrapper>
                     <label>
-                        <Input
+                        <input
                             type='checkbox'
                             checked={formData.sameAddress}
                             onChange={handleCheckboxChange}
-                            style={{ marginRight: '5px' }}
+                            className='me-2'
                         />
-                        Usar o mesmo endereço para a pessoa física
+                        Endereço físico e jurídico são iguais
                     </label>
                 </CheckboxWrapper>
-                <Button>ENVIAR</Button>
+                <Button type='submit'>Enviar</Button>
             </FormWrapper>
         </Container>
     )
